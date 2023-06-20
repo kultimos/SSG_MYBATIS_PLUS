8a4c70ee7c40709f613e1fac2f4cfeaa8992b869
wushuai <659-4gpe145ln@dingtalk.com>
2023-06-20 16:58:55 +0800

    p18:mybatis-plus中通用service接口的学习

---
+++ mybatis_plus/src/main/java/com/kul/service/UserService.java
@@ -0,0 +1,8 @@
+package com.kul.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.kul.pojo.User;
+
+public interface UserService extends IService<User> {
+
+}
---
+++ mybatis_plus/src/main/java/com/kul/service/impl/UserServiceImpl.java
@@ -0,0 +1,10 @@
+package com.kul.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.kul.mapper.UserMapper;
+import com.kul.pojo.User;
+import com.kul.service.UserService;
+
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+    
+}
